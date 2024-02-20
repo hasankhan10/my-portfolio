e0260806eed5d02b94f245f6cb4d702a2771335a
mehedi <darkhasan10@gmail.com>
2024-02-20 22:30:09 +0530

    just try to something to add but nothing happend

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -3,12 +3,12 @@ function Navbar() {
   return (
     <div className="navbar w-full h-[4rem] flex justify-center place-items-center absolute">
         <div className=" w-[42%] h-[3rem] bg-slate-300 mt-6 rounded-full fixed flex place-items-center justify-center space-x-20 text-slate-900 font-bold">
-          <h1>Home</h1>
-          <h1>About</h1>
-          <h1>Skill</h1>
-          <h1>Project</h1>
-          <h1>Contact</h1>
-          </div>
+            <h1>Home</h1>
+            <h1>About</h1>
+            <h1>Skill</h1>
+            <h1>Project</h1>
+            <h1>Contact</h1>
+        </div>
     </div>
   )
 }
