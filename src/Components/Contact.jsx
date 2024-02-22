6467e1eeec47f7228aae5e35f9b3c5f14653be9d
mehedi <darkhasan10@gmail.com>
2024-02-22 12:39:21 +0530

    addd resume section in contact page

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -22,6 +22,11 @@ function Contact() {
                         <h1 className=' text-xl font-semibold'>Email</h1>
                     </a>
 
+                    <a href="https://docs.google.com/document/d/1N2Z-YC1n0SQJ0WoX2yXMW497EH-DWEZBf4GRf6flBr8/edit#heading=h.5rf9wr4r3no2" target="_blank" className=' w-26 h-26 flex flex-col justify-center place-items-center cursor-pointer'>
+                        <img className=' w-14 h-14' src="https://cdn-icons-png.flaticon.com/512/9522/9522153.png" alt="" />
+                        <h1 className=' text-xl font-semibold'>Resume</h1>
+                    </a>
+
             </section>
         </section>
     </div>
