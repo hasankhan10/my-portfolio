171411ebb6e8bb564c219780d97ac4908776d456
mehedi <darkhasan10@gmail.com>
2024-02-24 18:54:52 +0530

    optimized hight and width in about page

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -2,8 +2,8 @@
 function About() {
   return (
     <div className=" w-full h-full flex justify-center place-items-center bg-slate-700">
-        <section className="about w-[90%] h-[55%] rounded-3xl flex justify-center place-items-center overflow-hidden sm:rounded-full sm:w-[50%] sm:h-[23%]">
-            <p className=" text-center m-8 text-2xl text-yellow-100 font-semibold leading-10 sm:leading-8">With 3 years of front-end and back-end development experience, I specialize in HTML, CSS, JavaScript, React, Next, express, mongoDB, creating visually engaging web applications with a user-centric focus</p>
+        <section className="about w-fit h-fit rounded-3xl flex justify-center place-items-center overflow-hidden sm:rounded-3xl sm:w-[50%] sm:h-[23%] p-1 m-4">
+            <p className=" text-center m-6 text-2xl text-yellow-100 font-semibold leading-10 sm:leading-8">With 3 years of front-end and back-end development experience, I specialize in HTML, CSS, JavaScript, React, Next, express, mongoDB, creating visually engaging web applications with a user-centric focus</p>
         </section>
     </div>
   )
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router-dom"
 function Navbar() {
   return (
     <div className="navbar w-full h-[4rem] flex justify-center place-items-center absolute z-50">
-        <div className=" w-[93%] h-[3rem] bg-slate-300 mt-6 sm:rounded-full rounded-2xl fixed flex place-items-center justify-center space-x-5 font-mono text-slate-900 font-bold sm:space-x-20 sm:w-[42%]">
+        <div className=" w-[98%] h-[3rem] bg-slate-300 mt-6 sm:rounded-full rounded-xl fixed flex place-items-center justify-center space-x-5 font-mono text-slate-900 font-bold sm:space-x-20 sm:w-[42%]">
             <NavLink to={"/"}>Home</NavLink >
             <NavLink to={"about"}>About</NavLink >
             <NavLink to={"skill"}>Skill</NavLink >
