6ae363f213797a6a3a9a2256cf354983e4046f28
mehedi <darkhasan10@gmail.com>
2024-02-24 19:51:49 +0530

    add transition in my all page

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import "./App.css"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
 import Home from "./Components/Home"
 import About from "./Components/About"
